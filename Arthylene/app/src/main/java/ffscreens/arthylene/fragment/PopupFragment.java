a90311d0401e95fc603b9a2a383dd4f5738bc2d6
Thibault44Screens <tn@44screens.com>
2017-07-11 11:06:35 +0200

    ARTHY-30 #start-progress button back and next on popup fragment

--- Arthylene/app/src/main/java/ffscreens/arthylene/fragment/PopupFragment.java
+++ Arthylene/app/src/main/java/ffscreens/arthylene/fragment/PopupFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -39,6 +40,8 @@ public class PopupFragment extends Fragment {
         ImageView image = view.findViewById(R.id.imageView2);
         TextView popup = view.findViewById(R.id.textPopup);
         TextView bottom = view.findViewById(R.id.textBottom);
+        Button back = view.findViewById(R.id.back);
+        Button next = view.findViewById(R.id.next);
 
 
         if (getArguments() != null) {
